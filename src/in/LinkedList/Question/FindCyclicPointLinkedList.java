e881d8e0260f2a34a0bb56f277197ba608183235
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-11-24 18:15:11 +0530

    find the cyclic point in linked list

---
+++ src/in/LinkedList/Question/FindCyclicPointLinkedList.java
@@ -0,0 +1,35 @@
+package in.LinkedList.Question;
+
+public class FindCyclicPointLinkedList {
+    public static int detectCyclicInLinkedList(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                    if (slow == fast) {
+                        return slow.data;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    static void main() {
+        Node node = new Node(1);
+        node.next = new Node(2);
+        node.next.next = new Node(3);
+        node.next.next.next = new Node(4);
+        node.next.next.next.next = new Node(5);
+        node.next.next.next.next.next = node.next;
+        int i = detectCyclicInLinkedList(node);
+        System.out.println(i);
+
+    }
+}
