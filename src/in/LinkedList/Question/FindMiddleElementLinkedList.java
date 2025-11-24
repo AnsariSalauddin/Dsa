6ab7ec9324ff7bb01590d0fc2cbbe496938efc69
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-11-24 18:00:10 +0530

    find the middle element of linked list

---
+++ src/in/LinkedList/Question/FindMiddleElementLinkedList.java
@@ -0,0 +1,24 @@
+package in.LinkedList.Question;
+
+public class FindMiddleElementLinkedList {
+    public static int middleElementLinkedList(Node node){
+        Node slow=node;
+        Node fast=node;
+
+        while (fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow.data;
+    }
+    static void main() {
+        Node node=new Node(1);
+        node.next=new Node(2);
+        node.next.next=new Node(3);
+        node.next.next.next=new Node(4);
+        node.next.next.next.next=new Node(5);
+        int i = middleElementLinkedList(node);
+        System.out.println(i);
+
+    }
+}
