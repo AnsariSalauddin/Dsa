da03feb59eebb9075aad7824f0559d415f8c0387
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-11-24 18:02:42 +0530

    detect the cyclic in linked list

---
+++ src/in/LinkedList/Question/DetectCyclicLinkedList.java
@@ -0,0 +1,26 @@
+package in.LinkedList.Question;
+
+public class DetectCyclicLinkedList {
+    public static boolean detectCyclicInLinkedList(Node head){
+        Node slow=head;
+        Node fast=head;
+        while (fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    static void main() {
+        Node node=new Node(1);
+        node.next=new Node(2);
+        node.next.next=new Node(3);
+        node.next.next.next=new Node(4);
+        node.next.next.next.next=new Node(5);
+        boolean b = detectCyclicInLinkedList(node);
+        System.out.println(b);
+
+    }
+}
